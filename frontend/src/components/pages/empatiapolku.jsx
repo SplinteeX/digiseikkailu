710918f0f37a95627475715b2b7f3908f18e41b6
SplinteeX <Jepsteri04@gmail.com>
2023-11-06 13:33:02 +0200

    fixed

--- frontend/src/components/pages/empatiapolku.jsx
+++ frontend/src/components/pages/empatiapolku.jsx
@@ -4,12 +4,12 @@ import suuntamerkki from "../../assets/suuntamerkki.webp";
 import maisema from "../../assets/maisemakuva.webp";
 import empatiadata from "../data/empatiaData";
 const empatiapolku = () => {
-    return (
-      <div className="empatiapolku">
-        <h1>Oppimistavoitteet</h1>
-          <div className="container">
-            <div className="oikeinmerkit">
-            {texts.map((empatiadata, index) => (
+  return (
+    <div className="empatiapolku">
+      <h1>Oppimistavoitteet</h1>
+      <div className="container">
+        <div className="oikeinmerkit">
+          {empatiadata.map((empatiadata, index) => (
             <Oikeinmerkki
               key={index}
               width="32px"
@@ -17,32 +17,29 @@ const empatiapolku = () => {
               teksti={empatiadata}
             ></Oikeinmerkki>
           ))}
-            </div>
+        </div>
 
-            <div className="right-div">
-
-            <div className="suunta">
-              <img src={suuntamerkki} height={550} width={623}></img>
-            </div>
-            <div className="vihapuhe">
-              <h1>Kuka jättää Tweetyn<br></br>
-                  viestiseinälle ilkeitä<br></br>
-                  kommentteja?<br></br><br></br>
-
-                  Mitä on vihapuhe ja miksi joku<br></br>
-                  kirjoittaa sitä?
-                  </h1>
-            </div>
-
-            </div>
+        <div className="right-div">
+          <div className="suunta">
+            <img src={suuntamerkki} height={550} width={623}></img>
           </div>
-          <div className="maisema">
-          <img src={maisema} height={550} width={1000}></img>
+          <div className="vihapuhe">
+            <h1>
+              Kuka jättää Tweetyn<br></br>
+              viestiseinälle ilkeitä<br></br>
+              kommentteja?<br></br>
+              <br></br>
+              Mitä on vihapuhe ja miksi joku<br></br>
+              kirjoittaa sitä?
+            </h1>
           </div>
+        </div>
       </div>
-      
-      
-    );
-  }
-  
-  export default empatiapolku;
+      <div className="maisema">
+        <img src={maisema} height={550} width={1000}></img>
+      </div>
+    </div>
+  );
+};
+
+export default empatiapolku;
