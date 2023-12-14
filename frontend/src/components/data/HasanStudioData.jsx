877dbe1223f63159643197558e452b1340fbb23a
YeahIHasProblems <samu.s.tolvanen@gmail.com>
2023-12-14 12:00:48 +0200

    välitallennus

---
+++ frontend/src/components/data/HasanStudioData.jsx
@@ -0,0 +1,16 @@
+export const HäsänStudioData = () => {
+    const Tehtävät = [
+      {
+        tehtNum: "1",
+        tehtName: "Tubettajan tietonurkka",
+        Kategoria: "HäsänStudio",
+        puolTeksti: "Tubetus, TikTokkaus, Snäppäys, hyödyt ja haitat – mitä kaikkea somejulkaisijan tulisi tietää?",
+        Kuva: "",
+        pieniTitle: "#sisällöntuottaminen #kuluttaja #seuraaja #tilaaja #koukuttavuus",
+        puolTeksti: "Tubettaja on YouTubeen ja usein myös muihin sosiaalisen median kanaviin sisältöä tekevä henkilö. Tubettajat ovat nousseet viime vuosina nuorten suurimmiksi idoleiksi joiden videoita katsotaan päivittäin satoja tunteja pelkästään Suomessa. Tubettajien sisältö vaihtelee omasta elämästä kertovista videoista erilaisiin aihepiireihin, kuten pelit, tekniikka, kauneus, meikit ja matkustaminen.",
+        puolTeksti: "Tubettajien seuraan ovat liittyneet myös TikTokkaajat, Snäppääjät sekä podcastaajat, joista ensimmäiset tekevät sisältöä sovellukseen ja jälkimmäiset tekevät radio-ohjelmien kaltaisia kokonaisuuksia nettiin."
+      },
+    ];
+  
+    return Tehtävät;
+  };
