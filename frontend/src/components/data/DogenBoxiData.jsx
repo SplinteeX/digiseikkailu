3376eba6cb337d5f79a825931ef128e8c011ab0f
YeahIHasProblems <samu.s.tolvanen@gmail.com>
2023-12-19 13:32:07 +0200

    välitallennus

--- frontend/src/components/data/DogenBoxiData.jsx
+++ frontend/src/components/data/DogenBoxiData.jsx
@@ -2,12 +2,55 @@ export const DogenBoxiData = () => {
   const Tehtävät = [
     {
       tehtNum: "1",
-      tehtName: "testi",
+      tehtName: "Uutisen rakenne",
+      Kategoria: "DogenBoxi",
+      title: [
+          "Harjoitellaan uutisten rakennetta",
+          "Kirjoita oma uutinen!",
+      ],
+      Kuvat: [
+          "/src/assets/DogeImages/uutisankkuri.webp",
+          "/src/assets/DogeImages/lokki.webp",
+      ],
+      puolTekstit: [
+          "Mieti aihe ja noudata ohjeita.",
+          "Lisää uutiskuva itse kuvaamalla tai poimi sopiva ja luvallinen kuva netistä.",
+          "Uutisen rakenne",
+          "Uutisessa tärkein asia kerrotaan jo otsikossa. Muut kappaleet antavat lisätietoa tapahtuneesta.",
+          "Uutinen vastaa uutiskysymyksiin seuraavassa järjestyksessä:",
+          "Otsikko",
+          "Ensimmäinen ja toinen kappale",
+          "Kolmas kappale",
+          "Neljäs kappale",
+          "Uutisen kappaleet ovat melko lyhyitä, kahdesta viiteen virkkeeseen pitkiä.",
+          "Uutinen voi sisältää tapahtumaan liittyvien henkilöiden haastatteluja. Tällöin henkilön nimi ja hänen osuutensa tapahtumaan (viranomainen, sivustakatsoja, osallinen) mainitaan ja kommentti kirjoitetaan repliikkiviivalla omalle rivilleen. Haastattelukysymyksiä ei kirjoiteta uutiseen.",
+      ],
+      numeroListat: [
+        "1. Mitä on tapahtunut?",
+        "2. Milloin on tapahtunut?",
+        "3. Missä on tapahtunut?",
+        "4. Kuka on tehnyt?",
+        "5. Miten on tapahtunut?",
+        "6. Miksi on tapahtunut?",
+        "7. Mitä taustatietoja tapahtumaan liittyy?",
+        "8. Mitä seurauksia tapahtumalla on?",
+      ],
+      Tekstit: [
+        "Uutisartikkeli:",
+        "“Nokkela Merilokki Yllätti Liuskasaaren Kouluoppilaat Suomenlinnassa”",
+        "Helsinki, 15.5.2025 (Toimittaja: Martti Mylläri, Stadin Lasten Sanomat)",
+        "Perjantaina 15. toukokuuta Helsingin Liuskasaaren koulun oppilaat joutuivat yllättävän tilanteen eteen retkeillessään Suomenlinnassa. Aurinkoisesta säästä ja mukavasta tunnelmasta huolimatta, retken evästauko sai odottamattoman käänteen, kun tunkeilija yllätti ryhmän.",
+        "Yllättäjä, joka myöhemmin tunnistettiin merilokiksi, nappasi nopeasti eväinä olleet ruuat 9-vuotiaan Liisa Männystön ja 10-vuotiaan Lasse Pöngän käsistä. Lokki vei mukanaan kalapuikkoeväät ja Trip-mehun suoraan lasten pusseista, jättäen heidät ja muut oppilaat hämmentyneiksi.",
+        "Kouluun liittyvien taustatietojen mukaan tämä sama lokki on aiemmin vohkinut kaulakoruja ja termospullon, jota se on käyttänyt kahvin juomiseen pesäkalliollaan.",
+        "Tapahtuman todistanut rehtori ilmaisi järkytyksensä tapahtuneesta. “Näin ei pitänyt käydä! Olin sanonut, että pussit pidetään kiinni,” rehtori mutisi.",
+        "Tämä outo tapahtuma on herättänyt keskustelua oppilaiden ja opettajien keskuudessa, ja se muistuttaa meitä luonnon yllättävyydestä – jopa keskellä kaupunkia.",
+      ],
+      kysymys: "",
     },
     {
       tehtNum: "2",
-      tehtName: "testi",
     },
   ];
+
   return Tehtävät;
 };
