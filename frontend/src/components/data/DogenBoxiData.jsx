679f49603ecee37067a989d58e3a1956ba116b7f
YeahIHasProblems <samu.s.tolvanen@gmail.com>
2023-12-19 21:48:42 +0200

    välitallennus

--- frontend/src/components/data/DogenBoxiData.jsx
+++ frontend/src/components/data/DogenBoxiData.jsx
@@ -2,14 +2,15 @@ export const DogenBoxiData = () => {
   const Tehtävät = [
     {
       tehtNum: "1",
-      tehtName: "Uutisen rakenne",
+      tehtName: "Harjoitellaan uutisten rakennetta",
       Kategoria: "DogenBoxi",
-      title: ["Harjoitellaan uutisten rakennetta", "Kirjoita oma uutinen!"],
+      smallTitle:
+        "Kirjoita oma uutinen!",
       Kuvat: [
         "/src/assets/DogeImages/uutisankkuri.webp",
         "/src/assets/DogeImages/lokki.webp",
       ],
-      puolTekstit: [
+      Tekstit: [
         "Mieti aihe ja noudata ohjeita.",
         "Lisää uutiskuva itse kuvaamalla tai poimi sopiva ja luvallinen kuva netistä.",
         "Uutisen rakenne",
@@ -32,20 +33,107 @@ export const DogenBoxiData = () => {
         "Mitä taustatietoja tapahtumaan liittyy?",
         "Mitä seurauksia tapahtumalla on?",
       ],
-      Tekstit: [
-        "Uutisartikkeli:",
-        "“Nokkela Merilokki Yllätti Liuskasaaren Kouluoppilaat Suomenlinnassa”",
-        "Helsinki, 15.5.2025 (Toimittaja: Martti Mylläri, Stadin Lasten Sanomat)",
-        "Perjantaina 15. toukokuuta Helsingin Liuskasaaren koulun oppilaat joutuivat yllättävän tilanteen eteen retkeillessään Suomenlinnassa. Aurinkoisesta säästä ja mukavasta tunnelmasta huolimatta, retken evästauko sai odottamattoman käänteen, kun tunkeilija yllätti ryhmän.",
-        "Yllättäjä, joka myöhemmin tunnistettiin merilokiksi, nappasi nopeasti eväinä olleet ruuat 9-vuotiaan Liisa Männystön ja 10-vuotiaan Lasse Pöngän käsistä. Lokki vei mukanaan kalapuikkoeväät ja Trip-mehun suoraan lasten pusseista, jättäen heidät ja muut oppilaat hämmentyneiksi.",
-        "Kouluun liittyvien taustatietojen mukaan tämä sama lokki on aiemmin vohkinut kaulakoruja ja termospullon, jota se on käyttänyt kahvin juomiseen pesäkalliollaan.",
-        "Tapahtuman todistanut rehtori ilmaisi järkytyksensä tapahtuneesta. “Näin ei pitänyt käydä! Olin sanonut, että pussit pidetään kiinni,” rehtori mutisi.",
-        "Tämä outo tapahtuma on herättänyt keskustelua oppilaiden ja opettajien keskuudessa, ja se muistuttaa meitä luonnon yllättävyydestä – jopa keskellä kaupunkia.",
+      Teksti:
+        "Uutisartikkeli: “Nokkela Merilokki Yllätti Liuskasaaren Kouluoppilaat Suomenlinnassa” Helsinki, 15.5.2025 (Toimittaja: Martti Mylläri, Stadin Lasten Sanomat) Perjantaina 15. toukokuuta Helsingin Liuskasaaren koulun oppilaat joutuivat yllättävän tilanteen eteen retkeillessään Suomenlinnassa. Aurinkoisesta säästä ja mukavasta tunnelmasta huolimatta, retken evästauko sai odottamattoman käänteen, kun tunkeilija yllätti ryhmän. Yllättäjä, joka myöhemmin tunnistettiin merilokiksi, nappasi nopeasti eväinä olleet ruuat 9-vuotiaan Liisa Männystön ja 10-vuotiaan Lasse Pöngän käsistä. Lokki vei mukanaan kalapuikkoeväät ja Trip-mehun suoraan lasten pusseista, jättäen heidät ja muut oppilaat hämmentyneiksi. Kouluun liittyvien taustatietojen mukaan tämä sama lokki on aiemmin vohkinut kaulakoruja ja termospullon, jota se on käyttänyt kahvin juomiseen pesäkalliollaan. Tapahtuman todistanut rehtori ilmaisi järkytyksensä tapahtuneesta. “Näin ei pitänyt käydä! Olin sanonut, että pussit pidetään kiinni,” rehtori mutisi. Tämä outo tapahtuma on herättänyt keskustelua oppilaiden ja opettajien keskuudessa, ja se muistuttaa meitä luonnon yllättävyydestä – jopa keskellä kaupunkia.",
+      kysymys: "Mitä elementtiä pidetään välttämättömänä hyvässä uutisessa?",
+      vaihtoehdot: [
+        "A) Henkilökohtaisia mielipiteitä",
+        "B) Taustatietoja ja yksityiskohtia tapahtumasta",
+        "C) Koristellut ja monimutkaiset kielikuvat",
+        "D) Mainostekstejä",
       ],
-      kysymys: "",
+      vastaus: "B) Taustatietoja ja yksityiskohtia tapahtumasta"
     },
     {
       tehtNum: "2",
+      tehtName: "Liian hyvää ollaakseen totta",
+      Kategoria: "DogenBoxi",
+      SoundCloud: "https://soundcloud.com/digiseikkailu-oy/8-liian-hyvaeae-ollakseen?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      SoundCloud: "https://soundcloud.com/digiseikkailu-oy/8-too-good-to-be-true?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      puolTeksti:
+        "Tarinassa käsitellään tiedon luotettavuuden arviointia sekä tutustutaan oikeisiin uutisiin ja valeuutisiin",
+      puolKuva: "/src/assets/DogeImages/jalkapallo.webp"
+      /* Tarina keskustelu eli kesken */
+    },
+    {
+      tehtNum: "3",
+      tehtName: "Tunnistetaan valeuutinen",
+      Kategoria: "DogenBoxi",
+      Kuvat: [
+        "/src/assets/DogeImages/lentävänorsu.webp",
+        "/src/assets/DogeImages/valeuutinen.webp",
+      ],
+      Tekstit: [
+        "Kuuluisa lentävä elefantti nähty Helsingin yllä – asiantuntijat hämmennyksissä! Helsinki, 1. huhtikuuta 2025",
+        "Tänään Helsingin taivaalla tapahtui jotain uskomatonta, kun lentävä elefantti ilmestyi pilvien joukkoon! Satojen ihmisten silminnäkijäkertomukset ovat vahvistaneet tämän hämmästyttävän ilmiön, joka on herättänyt ihmetystä ja riemua ympäri kaupunkia.",
+        "Kuvat ja videot lentävästä elefantista ovat levinneet nopeasti sosiaalisessa mediassa, ja monet ihmiset ovat kerääntyneet ulos ihailemaan tätä epätodellista näkyä. Elefantti on nähty lentävän korkealla taivaalla, liikkuen sulavasti ja ilman näkyviä siipiä tai muita lentoon tarvittavia välineitä.",
+        "Asiantuntijat ja tutkijat ovat seisseet sanattomina tämän ilmiön edessä, ja he tutkivat parhaillaan, miten tällainen ilmiö voisi olla mahdollinen. Yleinen teoria on, että tämä on yliluonnollinen tapahtuma, joka ei sovi perinteisiin selityksiin.",
+        "Kaupungin asukkaat ovat kuitenkin innoissaan tästä ihmeestä, ja monet uskovat sen olevan hyvä enne. Kaupunginjohtaja on jo ilmoittanut, että lentävä elefantti tullaan ottamaan vastaan juhlallisesti kaupungintalolla, ja päivä on julistettu viralliseksi juhlapäiväksi.",
+        "Tämä ainutlaatuinen tapahtuma jää historiankirjoihin Helsingin suurimpana ihmeenä, ja se muistuttaa meitä siitä, että maailma on täynnä yllätyksiä.",
+      ],
+      smallTitle: 
+        "Valeuutisen paljastus!",
+      Teksti: 
+        "Taitava median lukija on varuillaan lukiessaan uutisia ja etsiessään tietoa internetistä. Joskus voi nimittäin olla, että totena esitetty tieto ei pidäkään paikkaansa. Tällaisia uutishuijauksia kutsutaan valeuutisiksi.",
+      numeroLista: [
+        "Selvitä, kuka jutun on kirjoittanut. Oikeilla uutissivustoilla on yhteystiedot toimitukseen ja ainakin julkaisun päätoimittajan nimi. Jos uutisen yhteydessä ei mainita toimittajan nimeä tai toimituksen yhteystietoja, uutista voi epäillä valeuutiseksi.",
+        "Tutki uutisen kieltä. Oikeat uutiset kirjoitetaan asiallisella yleiskielellä. Hälytyskellot soivat, jos uutinen on kirjoitettu yllättävän humoristisesti ja jos siinä käytetään puhekielen ilmauksia tai muuta asiatyylistä poikkeavaa kieltä.",
+        "Varo vahvistusharhaa. Vahvistusharha tarkoittaa sitä, että me kaikki haluamme uskoa uutisia, jotka vahvistavat omaa käsitystämme asiasta. Esimerkiksi FC Barcelonan kannattajat haluavat todennäköisesti uskoa kaikki positiiviset uutiset joukkueesta, eivätkä välitä aina tarkastaa, pitääkö uutinen paikkaansa.",
+      ],
+      kysymys: "Mikä on hyvä tapa tunnistaa valeuutinen?",
+      vaihtoehdot: [
+        "A) Tarkistaa, onko uutinen suosittu sosiaalisessa mediassa",
+        "B) Tarkistaa uutisen lähde ja kirjoittaja",
+        "C) Valita uutinen sen perusteella, kuinka mielenkiintoinen se on",
+        "D) Uskoa uutinen, jos se on ystävän jakama",
+      ],
+      vastaus: "B) Tarkistaa uutisen lähde ja kirjoittaja"
+
+    },
+    {
+      tehtNum: "4",
+      tehtName: "Mediakupla",
+      Kategoria: "DogenBoxi",
+      /* Joku yle areena video puuttuu */
+      smallTitle: 
+        "Mikä on mediakupla?",
+      Tekstit: [
+        "Mediakupla tarkoittaa sitä, että kaikki toimintamme internetissä vaikuttaa siihen, millaista sisältöä saamme luettavaksi ja katsottavaksi. Monet mediasovellukset oppivat tulkitsemaan mediankäyttöämme ja tarjoavat nähtäväksemme aiheita, joista luulevat meidän pitävän. Siihen vaikuttavat sosiaalisen median käyttömme – tykkäykset, jaot jne. – sekä esimerkiksi hakukoneilla tekemämme haut.",
+        "Me kaikki elämme erilaisten mediakuplien sisällä. Hyvä esimerkki tästä on se, että jos kaksi henkilöä etsii samaa asiaa samalla hakusanalla Googlesta, he todennäköisesti saavat erilaisia hakutuloksia. Tämä johtuu heidän erilaisista mediakuplistaan – erilaisista hakuhistorioista ja erilaisesta toiminnasta sosiaalisessa mediassa",
+        "Selvitä oma mediakuplasi",
+        "Kokeilkaa itse! Päätä parin kanssa aihe, josta haluatte etsiä tietoa. Etsikää aiheesta tietoa Googlesta (esim. puhelimilla). Vertailkaa hakutuloksianne ovatko ne samanlaisia, vai eroavatko kaverisi hakutulokset aiempien hakujen perusteella. Entä sosiaalinen media? Katsokaa haluamanne median syötettä (feediä). Näyttääkö se samalta?",
+        "Pohdi, mitä vaikutuksia mediakuplalla voi olla.",
+        "Onko se mielestäsi hyvä vai huono asia?",
+        "Mitä ongelmia siitä voi aiheutua?",
+      ],
+      kysymys: "Mikä on mediakupla?",
+      vaihtoehdot: [
+        "A) Tieto, joka on aina totta",
+        "B) Sosiaalisen median ja hakukoneiden personoima sisältö",
+        "C) Vain negatiivisia uutisia sisältävä media",
+        "D) Kaikki internetissä julkaistu sisältö",
+      ],
+      vastaus: "B) Sosiaalisen median ja hakukoneiden personoima sisältö"
+    },
+    {
+      tehtNum: "5",
+      tehtName: "Disinformaatio",
+      Kategoria: "DogenBoxi",
+      Tekstit: [
+        "Etsi sanalle selitys",
+        "Disinformaatio (engl. disinformation) tarkoittaa tarkoituksellista harhaanjohtavaa tietoa tai tiedotusta. Alapuolelle on kerätty erilaisia disinformaatioon johtavia käsitteitä.",
+        "Raahaa sanat oikeisiin selityksiin.",
+      ],
+      /* pelilinkki */
+      kysymys: "Mitä disinformaatio tarkoittaa?",
+      vaihtoehdot: [
+        "A) Virheellistä tietoa vahingossa",
+        "B) Tarkoituksellisesti harhaanjohtavaa tietoa",
+        "C) Kaikki internetissä oleva tieto",
+        "D) Tieteellisiä faktoja",
+      ],
+      vastaus: "B) Tarkoituksellisesti harhaanjohtavaa tietoa"
+
     },
   ];
 
