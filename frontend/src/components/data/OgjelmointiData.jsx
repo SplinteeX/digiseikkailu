be5dbc3cb832951c828bfbbbbf48a65bc402beb1
Lenska5 <lenni.latvala@gmail.com>
2023-12-22 13:00:35 +0200

    ohjelmointiData tehty

--- frontend/src/components/data/OgjelmointiData.jsx
+++ frontend/src/components/data/OgjelmointiData.jsx
@@ -2,12 +2,178 @@ export const OhjelmointiData = () => {
   const Tehtävät = [
     {
       tehtNum: "1",
-      tehtName: "Laamanaitaus",
+      tehtName: "Käskyjä kummallisilla kielillä",
       Kategoria: "OhjelmointiPolku",
+      SoundClouds: [
+        "https://soundcloud.com/digiseikkailu-oy/23-kaskyja-kummallisilla-kielilla",
+        "https://soundcloud.com/digiseikkailu-oy/23-commands-in-strange-languages",
+      ],
+      Tekstit: [
+        "Tarinassa käsitellään<br><br> -ohjelmointikieli -käsitettä ja -tutustutaan lause -käsitteeseen ohjelmoinnissa.<br><br> Lisää aiheeseen liittyviä tehtäviä löydät Ohjelmointi -osiosta.",
+        "Häsä istui pihakeinussa ja oli rättiväsynyt. Hän oli juuri tullut koulusta kotiin. Viimeisellä tunnilla oli ollut äidinkieltä ja Markku-ope oli antanut erikoisen kotiläksyn. Appi tuli pihalle ja kysyi:",
+        "Appi - Miksi murjotat, Häsä? Eiköhän lähdetä Dogen kanssa lenkille?",
+        "Häsä - En voi, minulla on läksyt tekemättä. Pitää kirjoittaa viisi käskylausetta tietokoneella, Häsä huokaisi.",
+        "Appi ilahtui, tehtävähän kuulosti hauskalta.",
+        "Appi - Millä kielellä?, hän kysyi.",
+        "Häsä - Mitä tarkoitat? Suomeksi tietenkin, Häsä vastasi kummissaan.",
+        "Appi - No jos kirjoitat tietokoneelle lauseita eli käskyjä jollakin ohjelmointikielellä, saat sen tekemään mitä haluat. Se on ohjelmointia, koodausta, devausta!, Appi luennoi.",
+        "Appi - Lauseet kirjoitetaan rivi kerrallaan mahdollisimman tarkasti, jotta ohjelma toimii juuri halutulla tavalla. Lauseiden pitää myös olla oikeassa järjestyksessä. Silloin ohjelma toimii niin kuin haluamme.",
+        "Häsä - Hmm, mitähän kaikkea voisinkaan käskeä tietokoneen tekemään, jos osaisin ohjelmointikieltä, Häsä pohdiskeli.",
+        "Saisinkohan sen tekemään läksyt puolestani?",
+        "Appi - Ensin sinun pitää harjoitella jotain ohjelmointikieltä, Appi muistutti.",
+        "Appi - Mutta ihan ensimmäiseksi viedään Doge ulos. Sitten autan sinua niiden lauseiden kanssa. Vai mitä virkkeitä ne olivat. Mennään!",
+      ],
+      Kuva: "/src/assets/ohjelmointiImages/puu.webp",
     },
     {
-      tehtNum: "1",
-      tehtName: "Laamanaitaus",
+      tehtNum: "2",
+      tehtName: "Tervetuloa ohjelmointipolulle",
+      Kategoria: "OhjelmointiPolku",
+      mp4: "https://digiadventurers.com/wp-content/uploads/2019/12/Ohelmointi.mp4",
+      Kuvat: [
+        "/src/assets/ohjelmointiImages/voitoppia.webp",
+        "/src/assets/ohjelmointiImages/tekstit.webp",
+      ],
+    },
+    {
+      tehtNum: "3",
+      tehtName: "Mikä näistä on tietokone",
+      Kategoria: "OhjelmointiPolku",
+      Tekstit: [
+        "Alapuolella näet erilaisia kuvia. Yritä pohtia:<br><br> Mitkä ovat tietokoneita?<br><br> Mitkä eivät ole tietokoneita?<br><br> Keskustelkaa koko luokka/pari/ryhmä yhdessä ja perustelkaa vastauksenne.<br><br> puhelin / kamera / hissi / aurinko / henkilövaaka / mikroaaltouuni / kassakone / viherkasvi / sakset / vessanpönttö / korvanapit / sadepilvi / peliohjain…",
+        "Mistä voin tietää onko joku asia tietokone vai ei?<br><br> Jotta asiaa voi sanoa tietokoneeksi, siihen pitäisi kuulua:<br><br> ohjelmisto (“softa”, engl. software) on piilossa laitteen sisällä<br><br> laitteisto (“rauta”, engl. hardware) on ulkokuori<br><br> Tässä tehtävässä voit miettiä asiaa yksinkertaisesti. Pohdi, onko kuvan asiassa ulkokuoren lisäksi sisällä jokin ohjelmisto, johon on sisällytetty erilaisia käskyjä laitteelle. Onko joku ihminen ohjelmoinut laitteeseen tai asiaan ohjeet?<br><br> Jos kyseessä on laite, jonka sisällä on ohjelmisto, voit luokitella sen tässä tehtävässä tietokoneeksi.",
+      ],
+      Kuva: "/src/assets/ohjelmointiImages/asioita.webp",
+      kysymys:
+        "Mitkä seuraavista ovat tietokoneita tehtävän määritelmän mukaan?",
+      vaihtoehdot: [
+        "A. Puhelin ja kamera.",
+        "B. Aurinko ja sadepilvi.",
+        "C. Viherkasvi ja sakset.",
+        "D. Vessanpönttö ja korvanapit.",
+      ],
+      vastaus: "A. Puhelin ja kamera.",
+    },
+    {
+      tehtNum: "4",
+      tehtName: "Softaa ja rautaa",
+      Kategoria: "OhjelmointiPolku",
+      Kuva: "/src/assets/ohjelmointiImages/hardware.webp",
+      docs: "blob:https://drive.google.com/416b9040-f094-450b-9b71-b407d7e896db",
+      docsText: "Tulostettava lomake",
+      Teksti:
+        "Softaksi (ohjelmisto) sanotaan tietokoneen sisällä toimivaa ohjelmistoa, joka saa koneen toimimaan käskyillään.<br><br> Raudaksi (laitteisto) sanotaan ulospäin näkyvää, käsin kosketeltavaa ulkokuorta eli laitteistoa.<br><br> Ohjelmistoa ei voi käyttää ilman siihen sopivaa laitteistoa. Esimerkiksi tietokonepeliä ei voi pelata ilman näppäimistöä tai puhelinta. Ne tarvitsevat toisiaan!<br><br> Osaatko tunnistaa parit seuraavassa tehtävässä? Kuittaa tehtävä tehdyksi kun olet palauttanut sen opettajalle.",
+      puolTeksti:
+        "- hiiri<br><br> - peliohjain<br><br> - chatti<br><br> - tekstitiedosto<br><br> - tulostin<br><br> - osoitin<br><br> - puhelin<br><br> - nettipeli<br><br>",
+
+      puolKuva: "/src/assets/ohjelmointiImages/jotain.webp",
+      kysymys: "Mitä tarkoitetaan ohjelmistolla (“softalla”) tietokoneessa?",
+      vaihtoehdot: [
+        "A. Tietokoneen ulkokuorta.",
+        "B. Tietokoneen sisällä toimivaa ohjelmistoa.",
+        "C. Tietokoneen näyttöä.",
+        "D. Tietokoneen näppäimistöä.",
+      ],
+      vastaus: "B. Tietokoneen sisällä toimivaa ohjelmistoa.",
+    },
+    {
+      tehtNum: "5",
+      tehtName: "Etsitään tietokoneita",
+      Kategoria: "OhjelmointiPolku",
+      Tekstit: [
+        "Tarkkaile koodattua ympäristöäsi<br><br> Kaikki toimii nykyään koodilla. Kotonasi löydät koodilla toimivia laitteita yllättävän paljon: uunit, liedet, mikrot, televisiot, autot, lämpöpumput sisältävät nykyään prosessorin, joka suorittaa koodia.<br><br> Raportoi missä näit koneen ja mitä kone teki!",
+      ],
+      docs: "blob:https://drive.google.com/53d9356d-4eee-4908-9e63-d3bd84786f13",
+      docsText: "Tulostettava lomake",
+      Kuva: "/src/assets/ohjelmointiImages/lista.webp",
+      kysymys: "Mitä tarkoitetaan, kun sanotaan, että laite toimii “koodilla”?",
+      vaihtoehdot: [
+        "A. Se toimii manuaalisesti.",
+        "B. Se toimii automaattisesti ilman ihmisen apua.",
+        "C. Sillä on sisäinen prosessori, joka suorittaa ohjelmakoodia.",
+        "D. Se vaatii jatkuvaa huoltoa.",
+      ],
+      vastaus: "C. Sillä on sisäinen prosessori, joka suorittaa ohjelmakoodia.",
+    },
+    {
+      tehtNum: "6",
+      tehtName: "Värikoodiviittaus",
+      Kategoria: "OhjelmointiPolku",
+      Kuva: "/src/assets/ohjelmointiImages/lego.webp",
+      Teksti:
+        "Viittaamalla kerrot, että haluat kertoa jotain. Viittaus ei kuitenkaan kerro, mitä haluat sanoa. Viittaaminen sinänsä on jo algoritmi. Viittauksesta seuraa puheenvuoro.<br><br> Vaihtakaa viittausalgoritmi multilink-algoritmiin (muutkin palikat käyvät). Oppilaan tehtävä on siirtää viittauksen sijaan pulpetin etureunaan palikka, joka vastaa hänen tahtoaan.<br><br> Esimerkiksi jos oppilas haluaa kertoa oikean vastauksen, hän siirtää pulpetin etureunaan pinkin palikan.<br><br> Pinkki palikka = Haluan vastata<br><br> Keltainen palikka = Haluan kysyä jotain<br><br> Vihreä palikka = Olen samaa mieltä<br><br> Punainen palikka = Olen eri mieltä",
+    },
+    {
+      tehtNum: "7",
+      tehtName: "Värikoodiviittaus",
+      Kategoria: "OhjelmointiPolku",
+      Teksti:
+        "Auta Doge, Häsä ja Laama maaliin! Lisää viivoille ohjeet.<br><br> Esimerkki:<br><br> Liiku 9 ruutua<br><br> Käänny oikealle. Käänny vasemmalle.<br><br> Paina lopuksi  nappia.",
+      Unity:
+        "https://digiadventurers.com/unity-games/prod/ohjelmointi1/index.html",
+    },
+    {
+      tehtNum: "8",
+      tehtName: "Anna ohjeet hahmoille joilla he löytävät hävinneen esineen.",
+      Kategoria: "OhjelmointiPolku",
+      Tekstit: [
+        "Laama - Haluan kukan luokse!",
+        "Häsä - Olen hävittänyt videokamerani!",
+        "Doge - Minne piilotin luuni?",
+        "Esim.<br><br> Liiku 9 eteenpäin.<br><br> Käänny vasemmalle.<br><br> Liiku 3 eteenpäin.<br><br> Paina lopuksi 'Suorita' näppäintä.",
+      ],
+      Unity:
+        "https://digiadventurers.com/unity-games/prod/ohjelmointi2/index.html",
+    },
+    {
+      tehtNum: "9",
+      tehtName: "Äänellä ohjelmointia",
+      Kategoria: "OhjelmointiPolku",
+      puolTekstit: [
+        "Toinen parista kävelee luokkaan, jossa on pulpeteista rakennettu labyrintti. Tehtävänä on päästä labyrintti läpi. Labyrintin pitää olla sellainen, että siinä on useampi tapa edetä loppuun saakka. Kävelijan silmät on peitetty. Toinen parista antaa kävelijälle ohjekoodeja. Koodit tulevat tällä kertaa itse tehtyjen äänien muodossa. Luokkatilassa kannattaa varmistua, ettei reitin lähelle jää esineitä, joihin kaatuessaan voi loukata itsensä.",
+        "kävele eteenpäin 2 askelta =",
+        "käänny vasempaan =",
+        "käänny oikeaan =",
+        "peruuta kävellen 1 askelta =",
+        "pysähdy =",
+      ],
+      puolKuvat: [
+        "/src/assets/ohjelmointiImages/megafoni.webp",
+        "/src/assets/ohjelmointiImages/clap.webp",
+        "/src/assets/ohjelmointiImages/pow.webp",
+        "/src/assets/ohjelmointiImages/puff.webp",
+        "/src/assets/ohjelmointiImages/oops.webp",
+        "/src/assets/ohjelmointiImages/bang.webp",
+      ],
+      Teksti: "Äänet toimivat nyt algoritmeinä",
+    },
+    {
+      tehtNum: "10",
+      tehtName: "Lue tarina Dogen herkkuleivän valmistuksesta.",
+      Kategoria: "OhjelmointiPolku",
+      Kuvat: [
+        "/src/assets/ohjelmointiImages/saisiolla.webp",
+        "/src/assets/ohjelmointiImages/burgeri.webp",
+        "/src/assets/ohjelmointiImages/digiway.webp",
+      ],
+      Tekstit: [
+        "1.<br><br> 2.<br><br> 3.<br><br> 4.<br><br> 5. ….",
+        "Lisää lopuksi SUORITA -käsky.<br><br> Kokeile kaverin kanssa leikkien, toimisiko ohjekoodisi.<br><br> Palauta ohjekoodi kirjoitettuna opettajalle.",
+        "Dogen herkkuleipä Kaikkihan tietävät koirien olevan aikamoisia herkkusuita ja näin on asian laita myös Dogen kohdalla. Dogen viikon kohokohta on, kun hän pääsee Apin kanssa Digiway-sämpyläravintolaan, jossa hän saa tilata juuri sellaisen sämpylän, kun kulloinkin haluaa. Sattumoisin tänään onkin Dogen nimipäivä ja Appi on luvannut viedä Dogen tämän lempiravintolaan.<br><br> Huomattavaa tässä ravintolassa on se, että jokainen asiakas ohjelmoi sämpylärobotin toimimaan juuri haluamallaan tavalla, jotta tilattu leipä on juuri halutun kaltainen. Tämä vaatiikin huomattavaa tarkkuutta ja ohjeiden antamista mahdollisimman selkeällä tavalla. Doge saapuu Apin kanssa Digiway-sämpyläravintolaan ja kohtaa heti sisään päästyään suurella kosketusnäytöllä varustetun tilausautomaatin.<br><br> – ”Tervetuloa Digiway-ravintolaan.” Tilausautomaatti toivottaa hieman robottimaisella äänellä.<br><br> – ”Ole hyvä ja valitse näytöltä haluamasi sämpylä, täytteet sekä niiden kokoamisjärjestys.”<br><br> Doge tutkailee hetken tilausautomaatin huiman laajaa valikoimaa ja alkaa pian antamaan koneelle ohjeita herkkusämpylänsä kokoamista varten.",
+        "Lopuksi Doge painaa tilausautomaatin ”Suorita”-nappia seuraa ihmeisään, kuinka Digiwayn sämpyläkone valmistaa sämpylän juuri Dogen ohjeiden mukaan. Sämpylä näyttää valmistuvan kuin itsestään Dogen odottaessa ruokaansa tiskillä.<br><br> Pian sämpyläkoneen teleskooppikoura ojentaakin valmiin sämpylän Dogelle:<br><br> – ”Olkaa hyvä. Tilauksenne on nyt valmis. Saisiko olla vielä muuta?”, robotti kysyy ystävällisesti Dogelta.<br><br> – ”Kiitos teille oikein paljon. Lasi jäävettä voisi maistua herkkuleivän kanssa.”, Doge vastasi.<br><br> Ja silmänräpäyksessä robotti toi Dogen tarjottimelle ison mukillisen jäävettä ja jätti tämän jälkeen ystävämme nauttimaan hyvin ansaitusta herkkuleivästään.",
+      ],
+      numeroLista: [
+        "Valitse sämpyläksi täysjyväsämpylä.",
+        "Leikkaa sämpylä kahtia ja paahda molemmat puolet rapeiksi.",
+        "Lisää molemmille sämpylän puoliskoille ohut kerros herkkuluun makuista majoneesia.",
+        "Lisää sämpylöiden väliin kaksi härkäpapupihviä.",
+        "Pilko kuusi yhtä paksua palaa kurkkua ja lisää kurkut pihvien päälle.",
+        "Paista seuraavaksi rapeaksi neljä sipulirengasta ja lisää ne kurkkujen päälle.",
+        "Pese nyt huolellisesti kaksi isoa salaatinlehteä.",
+        "Ripottele päälle vielä hyppysellinen mustapippuria.",
+        "Aseta nyt sämpylän kansi pohjan ja täytteiden päälle.",
+        "Kiedo valmis sämpylä paperiin syömisen helpottamiseksi.",
+      ],
     },
   ];
   return Tehtävät;
