1a93d1ac168ab67571c0bef5186abc2f955db257
Jesper <88264190+SplinteeX@users.noreply.github.com>
2023-12-22 13:45:16 +0200

    ++

--- frontend/src/components/pages/Maps/LaamanAitausMap.jsx
+++ frontend/src/components/pages/Maps/LaamanAitausMap.jsx
@@ -10,13 +10,13 @@ export const LaamanAitausMap = () => {
           Balls: [
             {
               Väri: "Vihreä",
-              Kategoria: "Tietoteksti",
+              Kategoria: "Tarina",
               number: "1",
               x: 385,
               y: 777,
             },
             {
-              Väri: "Rosa",
+              Väri: "Vihreä",
               Kategoria: "Tehtävä",
               number: "2",
               x: 785,
@@ -24,42 +24,42 @@ export const LaamanAitausMap = () => {
             },
             {
               Väri: "Vihreä",
-              Kategoria: "Tarina",
+              Kategoria: "Tehtävä",
               number: "3",
               x: 610,
               y: 575,
             },
             {
               Väri: "Vihreä",
-              Kategoria: "Video",
+              Kategoria: "Tarina",
               number: "4",
               x: 550,
               y: 495,
             },
             {
-              Väri: "Rosa",
-              Kategoria: "Tietoteksti",
+              Väri: "Vihreä",
+              Kategoria: "Tarina",
               number: "5",
               x: 775,
               y: 475,
             },
             {
               Väri: "Vihreä",
-              Kategoria: "Tarina",
+              Kategoria: "Tehtävä",
               number: "6",
               x: 950,
               y: 413,
             },
             {
               Väri: "Vihreä",
-              Kategoria: "Tietoteksti",
+              Kategoria: "Tarina",
               number: "7",
               x: 630,
               y: 395,
             },
             {
               Väri: "Vihreä",
-              Kategoria: "Tietoteksti",
+              Kategoria: "Tarina",
               number: "8",
               x: 530,
               y: 320,
@@ -73,21 +73,35 @@ export const LaamanAitausMap = () => {
             },
             {
               Väri: "Vihreä",
-              Kategoria: "Tietoteksti",
+              Kategoria: "Tehtävä",
               number: "10",
               x: 760,
               y: 215,
             },
             {
-              Väri: "Sininen",
-              Kategoria: "Tietoteksti",
+              Väri: "Vihreä",
+              Kategoria: "Tehtävä",
               number: "11",
-              x: 500,
-              y: 120,
+              x: 550,
+              y: 200,
+            },
+            {
+              Väri: "Rosa",
+              Kategoria: "Tehtävä",
+              number: "12",
+              x: 350,
+              y: 165,
+            },
+            {
+              Väri: "Rosa",
+              Kategoria: "Tietoteksti",
+              number: "13",
+              x: 700,
+              y: 105,
             },
           ],
         }}
-        url="/tehtävät/laamaaitaus"
+        url="/tehtävät/laamanaitaus"
       />
     </>
   );
