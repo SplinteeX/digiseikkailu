57a731399fd4bc0a923137a10c434002edbd6c9f
Jesper <88264190+SplinteeX@users.noreply.github.com>
2023-12-31 15:36:24 +0200

    ++

--- frontend/src/components/pages/empatiapolku.jsx
+++ frontend/src/components/pages/empatiapolku.jsx
@@ -3,11 +3,24 @@ import Oikeinmerkki from "../elements/empatiapolku/Listatekstit";
 import { EmpatiaData } from "../data/empatiaData";
 import SuuntaMerkki from "../elements/empatiapolku/suuntaMerkki";
 import suuntamerkkikuva from "../../assets/suuntamerkki.webp";
-
+import maisema from "../../assets/maisemakuva.webp";
+const vuoropuhelut = [
+  {
+    kuva: "/src//assets/Puhuja1.webp",
+    teksti:
+      "— Moi! Olen Häsä. Tässä on paras kaverini Appi. Tämä on Somen kaupunki!",
+  },
+  {
+    kuva: "/src/assets/Puhuja2.webp",
+    teksti:
+      "— Tule, näytämme sinulle vähän paikkoja ja asukkaita. Tapaat Tweetyn, Dogen, Laaman, Miuku-Maukun ja Kursorin.",
+  },
+];
 export const EmpatiaPolku = () => {
   return (
     <div className="empatiapolku">
-      <h1>Oppimistavoitteet</h1>
+      <h1>Empatiapolku</h1>
+      <h2>Oppimistavoitteet</h2>
       <div className="uppersection">
         <div className="oikeinmerkit">
           {EmpatiaData.map((item, index) => (
@@ -21,6 +34,23 @@ export const EmpatiaPolku = () => {
             kuva={suuntamerkkikuva}
             teksti="Kuka jättää Tweetyn viestiseinälle ilkeitä kommentteja? Mitä on vihapuhe ja miksi joku kirjoittaa sitä?"
           ></SuuntaMerkki>
+        </div>
+      </div>
+      <div className="lowersection">
+        <img src={maisema} alt="" />
+        {vuoropuhelut.map((item, index) => (
+          <div className="vuoropuhelu">
+            <img src={item.kuva} width={"100px"} height={"100px"} alt="" />
+            <p>{item.teksti}</p>
+          </div>
+        ))}
+        <div className="Hämis">
+          <img
+            src="/src/assets/hämis.webp"
+            width={"200px"}
+            height={"150px"}
+            alt=""
+          />
         </div>
       </div>
     </div>
