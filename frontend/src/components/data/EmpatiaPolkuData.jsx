b8a173032cb2f6fc997b92b3ae296db6b11da72c
Lenska5 <lenni.latvala@gmail.com>
2024-01-03 14:37:36 +0200

    +

---
+++ frontend/src/components/data/EmpatiaPolkuData.jsx
@@ -0,0 +1,45 @@
+export const EmpatiaPolkuData = () => {
+  const Tehtävät = [
+    {
+      tehtNum: "1",
+      tehtName: "Peukkutehtävä 1",
+      Kuvat: ["/src/assets/peukkuohjeet.webp", "/src/assets/papukaija"],
+      Tekstit: [
+        "Tweetyn viestiseinä",
+        "Tässä on Tweety. Se hallinnoi kaupungin viestiseinää ja on laajan lintuverkoston ylläpitäjä. Ylläpitäjän tehtäviin kuuluu viestien tarkkaileminen ja sääntöjen vastaisten viestien poistaminen.",
+      ],
+    },
+    {
+      tehtNum: "2",
+      tehtName: "Tehtävä 2",
+      videoUrl:
+        "https://digiadventurers.com/wp-content/uploads/2019/10/720p-kopio.mov",
+      Tekstit: [
+        "Ota selvää mistä asiasta Tweety oli huolissaan?",
+        "Tweety - Osaatteko sanoa, kuka tämän viestin on jättänyt tänne? Tweety kysyy kulmahöyhenet kurtussa.",
+        "Häsä - Hmm.. katsotaan. En tunnista käsialaa, mutta tällaisen viestin ei pitäisi olla täällä kaikkien nähtävillä. Sehän on ilkeä, Häsä sanoo.",
+        "Appi - Tämähän ei ole muuten edes ensimmäinen kerta! Poistetaan lappu, mutta pidetään se tallessa. Voimme tutkia sitä tarkemmin, jos pyydämme Dogen mukaan, Appi sanoo.",
+        "Tweety - Ettehän silti anna typerien viestin pilata päivää. Palataan! Tweety visertää ja pyrähtää lintuverkoston kokoukseen.",
+      ],
+    },
+    {
+      tehtNum: "3",
+      tehtName: "Tehtävä 3",
+      Teksti: "Mitkä viestit sinä poistaisit seinältä?",
+      Kuvat: [
+        "/src/assets/peukkuohjeet.webp/hämis.webp",
+        "/src/assets/peukkuohjeet.webp/1.webp",
+        "/src/assets/peukkuohjeet.webp/2.webp",
+        "/src/assets/peukkuohjeet.webp/3.webp",
+        "/src/assets/peukkuohjeet.webp/4.webp",
+        "/src/assets/peukkuohjeet.webp/5.webp",
+        "/src/assets/peukkuohjeet.webp/6.webp",
+      ],
+    },
+    {
+      tehtNum: "4",
+      tehtName: "Tehtävä 4",
+      youtube: "VCqX0G-hQDo",
+    },
+  ];
+};
