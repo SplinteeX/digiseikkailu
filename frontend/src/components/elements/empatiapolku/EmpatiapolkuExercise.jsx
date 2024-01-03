e7cda144e89b7136c8ca296ee30c177dc2810b58
Lenska5 <lenni.latvala@gmail.com>
2024-01-03 14:23:01 +0200

    +

--- frontend/src/components/elements/empatiapolku/EmpatiapolkuExercise.jsx
+++ frontend/src/components/elements/empatiapolku/EmpatiapolkuExercise.jsx
@@ -1,3 +1,28 @@
 import YoutubeVideo from "../elements/YoutubeVideo";
 import { Link } from "react-router-dom";
-export const EmpatiapolkuExercise = () => {};
+
+export const EmpatiapolkuExercise = ({ Data }) => {
+  return (
+    <div>
+      {Data.Kuva && (
+        <div className="Image-container">
+          <img className="imagefull" src={Data.Kuva} alt="Image" />
+        </div>
+      )}
+
+      {Data.VideoTeht && (
+        <div className="videotehtävä">
+          <div className="leftSide">
+            <p>TEHTÄVÄ 2</p>
+            <img src="../../../assets/kissakuva" alt="Kissakuva" />
+            <p>Ota selvää mistä asiasta Tweety oli huolissaan?</p>
+          </div>
+          <div className="rightSide">
+            <video src={Data.videoUrl}></video>
+          </div>
+        </div>
+      )}
+      {Data.Tekstit && <p>{Data.Tekstit}</p>}
+    </div>
+  );
+};
