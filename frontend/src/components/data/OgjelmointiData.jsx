60736165fda6e1387fed3cc794f5127cc11d17bd
YeahIHasProblems <samu.s.tolvanen@gmail.com>
2024-01-08 14:06:47 +0200

    br to \n

--- frontend/src/components/data/OgjelmointiData.jsx
+++ frontend/src/components/data/OgjelmointiData.jsx
@@ -9,7 +9,7 @@ export const OhjelmointiData = () => {
         "https://soundcloud.com/digiseikkailu-oy/23-commands-in-strange-languages",
       ],
       Tekstit: [
-        "Tarinassa käsitellään<br><br> -ohjelmointikieli -käsitettä ja -tutustutaan lause -käsitteeseen ohjelmoinnissa.<br><br> Lisää aiheeseen liittyviä tehtäviä löydät Ohjelmointi -osiosta.",
+        "Tarinassa käsitellään\n\n -ohjelmointikieli -käsitettä ja -tutustutaan lause -käsitteeseen ohjelmoinnissa.\n\n Lisää aiheeseen liittyviä tehtäviä löydät Ohjelmointi -osiosta.",
         "Häsä istui pihakeinussa ja oli rättiväsynyt. Hän oli juuri tullut koulusta kotiin. Viimeisellä tunnilla oli ollut äidinkieltä ja Markku-ope oli antanut erikoisen kotiläksyn. Appi tuli pihalle ja kysyi:",
         "Appi - Miksi murjotat, Häsä? Eiköhän lähdetä Dogen kanssa lenkille?",
         "Häsä - En voi, minulla on läksyt tekemättä. Pitää kirjoittaa viisi käskylausetta tietokoneella, Häsä huokaisi.",
@@ -40,8 +40,8 @@ export const OhjelmointiData = () => {
       tehtName: "Mikä näistä on tietokone",
       Kategoria: "OhjelmointiPolku",
       Tekstit: [
-        "Alapuolella näet erilaisia kuvia. Yritä pohtia:<br><br> Mitkä ovat tietokoneita?<br><br> Mitkä eivät ole tietokoneita?<br><br> Keskustelkaa koko luokka/pari/ryhmä yhdessä ja perustelkaa vastauksenne.<br><br> puhelin / kamera / hissi / aurinko / henkilövaaka / mikroaaltouuni / kassakone / viherkasvi / sakset / vessanpönttö / korvanapit / sadepilvi / peliohjain…",
-        "Mistä voin tietää onko joku asia tietokone vai ei?<br><br> Jotta asiaa voi sanoa tietokoneeksi, siihen pitäisi kuulua:<br><br> ohjelmisto (“softa”, engl. software) on piilossa laitteen sisällä<br><br> laitteisto (“rauta”, engl. hardware) on ulkokuori<br><br> Tässä tehtävässä voit miettiä asiaa yksinkertaisesti. Pohdi, onko kuvan asiassa ulkokuoren lisäksi sisällä jokin ohjelmisto, johon on sisällytetty erilaisia käskyjä laitteelle. Onko joku ihminen ohjelmoinut laitteeseen tai asiaan ohjeet?<br><br> Jos kyseessä on laite, jonka sisällä on ohjelmisto, voit luokitella sen tässä tehtävässä tietokoneeksi.",
+        "Alapuolella näet erilaisia kuvia. Yritä pohtia:\n\n Mitkä ovat tietokoneita?\n\n Mitkä eivät ole tietokoneita?\n\n Keskustelkaa koko luokka/pari/ryhmä yhdessä ja perustelkaa vastauksenne.\n\n puhelin / kamera / hissi / aurinko / henkilövaaka / mikroaaltouuni / kassakone / viherkasvi / sakset / vessanpönttö / korvanapit / sadepilvi / peliohjain…",
+        "Mistä voin tietää onko joku asia tietokone vai ei?\n\n Jotta asiaa voi sanoa tietokoneeksi, siihen pitäisi kuulua:\n\n ohjelmisto (“softa”, engl. software) on piilossa laitteen sisällä\n\n laitteisto (“rauta”, engl. hardware) on ulkokuori\n\n Tässä tehtävässä voit miettiä asiaa yksinkertaisesti. Pohdi, onko kuvan asiassa ulkokuoren lisäksi sisällä jokin ohjelmisto, johon on sisällytetty erilaisia käskyjä laitteelle. Onko joku ihminen ohjelmoinut laitteeseen tai asiaan ohjeet?\n\n Jos kyseessä on laite, jonka sisällä on ohjelmisto, voit luokitella sen tässä tehtävässä tietokoneeksi.",
       ],
       Kuva: "/src/assets/ohjelmointiImages/asioita.webp",
       kysymys:
@@ -62,9 +62,9 @@ export const OhjelmointiData = () => {
       docs: "blob:https://drive.google.com/416b9040-f094-450b-9b71-b407d7e896db",
       docsText: "Tulostettava lomake",
       Teksti:
-        "Softaksi (ohjelmisto) sanotaan tietokoneen sisällä toimivaa ohjelmistoa, joka saa koneen toimimaan käskyillään.<br><br> Raudaksi (laitteisto) sanotaan ulospäin näkyvää, käsin kosketeltavaa ulkokuorta eli laitteistoa.<br><br> Ohjelmistoa ei voi käyttää ilman siihen sopivaa laitteistoa. Esimerkiksi tietokonepeliä ei voi pelata ilman näppäimistöä tai puhelinta. Ne tarvitsevat toisiaan!<br><br> Osaatko tunnistaa parit seuraavassa tehtävässä? Kuittaa tehtävä tehdyksi kun olet palauttanut sen opettajalle.",
+        "Softaksi (ohjelmisto) sanotaan tietokoneen sisällä toimivaa ohjelmistoa, joka saa koneen toimimaan käskyillään.\n\n Raudaksi (laitteisto) sanotaan ulospäin näkyvää, käsin kosketeltavaa ulkokuorta eli laitteistoa.\n\n Ohjelmistoa ei voi käyttää ilman siihen sopivaa laitteistoa. Esimerkiksi tietokonepeliä ei voi pelata ilman näppäimistöä tai puhelinta. Ne tarvitsevat toisiaan!\n\n Osaatko tunnistaa parit seuraavassa tehtävässä? Kuittaa tehtävä tehdyksi kun olet palauttanut sen opettajalle.",
       puolTeksti:
-        "- hiiri<br><br> - peliohjain<br><br> - chatti<br><br> - tekstitiedosto<br><br> - tulostin<br><br> - osoitin<br><br> - puhelin<br><br> - nettipeli<br><br>",
+        "- hiiri\n\n - peliohjain\n\n - chatti\n\n - tekstitiedosto\n\n - tulostin\n\n - osoitin\n\n - puhelin\n\n - nettipeli\n\n",
 
       puolKuva: "/src/assets/ohjelmointiImages/jotain.webp",
       kysymys: "Mitä tarkoitetaan ohjelmistolla (“softalla”) tietokoneessa?",
@@ -81,7 +81,7 @@ export const OhjelmointiData = () => {
       tehtName: "Etsitään tietokoneita",
       Kategoria: "OhjelmointiPolku",
       Tekstit: [
-        "Tarkkaile koodattua ympäristöäsi<br><br> Kaikki toimii nykyään koodilla. Kotonasi löydät koodilla toimivia laitteita yllättävän paljon: uunit, liedet, mikrot, televisiot, autot, lämpöpumput sisältävät nykyään prosessorin, joka suorittaa koodia.<br><br> Raportoi missä näit koneen ja mitä kone teki!",
+        "Tarkkaile koodattua ympäristöäsi\n\n Kaikki toimii nykyään koodilla. Kotonasi löydät koodilla toimivia laitteita yllättävän paljon: uunit, liedet, mikrot, televisiot, autot, lämpöpumput sisältävät nykyään prosessorin, joka suorittaa koodia.\n\n Raportoi missä näit koneen ja mitä kone teki!",
       ],
       docs: "blob:https://drive.google.com/53d9356d-4eee-4908-9e63-d3bd84786f13",
       docsText: "Tulostettava lomake",
@@ -101,14 +101,14 @@ export const OhjelmointiData = () => {
       Kategoria: "OhjelmointiPolku",
       Kuva: "/src/assets/ohjelmointiImages/lego.webp",
       Teksti:
-        "Viittaamalla kerrot, että haluat kertoa jotain. Viittaus ei kuitenkaan kerro, mitä haluat sanoa. Viittaaminen sinänsä on jo algoritmi. Viittauksesta seuraa puheenvuoro.<br><br> Vaihtakaa viittausalgoritmi multilink-algoritmiin (muutkin palikat käyvät). Oppilaan tehtävä on siirtää viittauksen sijaan pulpetin etureunaan palikka, joka vastaa hänen tahtoaan.<br><br> Esimerkiksi jos oppilas haluaa kertoa oikean vastauksen, hän siirtää pulpetin etureunaan pinkin palikan.<br><br> Pinkki palikka = Haluan vastata<br><br> Keltainen palikka = Haluan kysyä jotain<br><br> Vihreä palikka = Olen samaa mieltä<br><br> Punainen palikka = Olen eri mieltä",
+        "Viittaamalla kerrot, että haluat kertoa jotain. Viittaus ei kuitenkaan kerro, mitä haluat sanoa. Viittaaminen sinänsä on jo algoritmi. Viittauksesta seuraa puheenvuoro.\n\n Vaihtakaa viittausalgoritmi multilink-algoritmiin (muutkin palikat käyvät). Oppilaan tehtävä on siirtää viittauksen sijaan pulpetin etureunaan palikka, joka vastaa hänen tahtoaan.\n\n Esimerkiksi jos oppilas haluaa kertoa oikean vastauksen, hän siirtää pulpetin etureunaan pinkin palikan.\n\n Pinkki palikka = Haluan vastata\n\n Keltainen palikka = Haluan kysyä jotain\n\n Vihreä palikka = Olen samaa mieltä\n\n Punainen palikka = Olen eri mieltä",
     },
     {
       tehtNum: "7",
       tehtName: "Värikoodiviittaus",
       Kategoria: "OhjelmointiPolku",
       Teksti:
-        "Auta Doge, Häsä ja Laama maaliin! Lisää viivoille ohjeet.<br><br> Esimerkki:<br><br> Liiku 9 ruutua<br><br> Käänny oikealle. Käänny vasemmalle.<br><br> Paina lopuksi  nappia.",
+        "Auta Doge, Häsä ja Laama maaliin! Lisää viivoille ohjeet.\n\n Esimerkki:\n\n Liiku 9 ruutua\n\n Käänny oikealle. Käänny vasemmalle.\n\n Paina lopuksi  nappia.",
       Unity:
         "https://digiadventurers.com/unity-games/prod/ohjelmointi1/index.html",
     },
@@ -120,7 +120,7 @@ export const OhjelmointiData = () => {
         "Laama - Haluan kukan luokse!",
         "Häsä - Olen hävittänyt videokamerani!",
         "Doge - Minne piilotin luuni?",
-        "Esim.<br><br> Liiku 9 eteenpäin.<br><br> Käänny vasemmalle.<br><br> Liiku 3 eteenpäin.<br><br> Paina lopuksi 'Suorita' näppäintä.",
+        "Esim.\n\n Liiku 9 eteenpäin.\n\n Käänny vasemmalle.\n\n Liiku 3 eteenpäin.\n\n Paina lopuksi 'Suorita' näppäintä.",
       ],
       Unity:
         "https://digiadventurers.com/unity-games/prod/ohjelmointi2/index.html",
@@ -157,10 +157,10 @@ export const OhjelmointiData = () => {
         "/src/assets/ohjelmointiImages/digiway.webp",
       ],
       Tekstit: [
-        "1.<br><br> 2.<br><br> 3.<br><br> 4.<br><br> 5. ….",
-        "Lisää lopuksi SUORITA -käsky.<br><br> Kokeile kaverin kanssa leikkien, toimisiko ohjekoodisi.<br><br> Palauta ohjekoodi kirjoitettuna opettajalle.",
-        "Dogen herkkuleipä Kaikkihan tietävät koirien olevan aikamoisia herkkusuita ja näin on asian laita myös Dogen kohdalla. Dogen viikon kohokohta on, kun hän pääsee Apin kanssa Digiway-sämpyläravintolaan, jossa hän saa tilata juuri sellaisen sämpylän, kun kulloinkin haluaa. Sattumoisin tänään onkin Dogen nimipäivä ja Appi on luvannut viedä Dogen tämän lempiravintolaan.<br><br> Huomattavaa tässä ravintolassa on se, että jokainen asiakas ohjelmoi sämpylärobotin toimimaan juuri haluamallaan tavalla, jotta tilattu leipä on juuri halutun kaltainen. Tämä vaatiikin huomattavaa tarkkuutta ja ohjeiden antamista mahdollisimman selkeällä tavalla. Doge saapuu Apin kanssa Digiway-sämpyläravintolaan ja kohtaa heti sisään päästyään suurella kosketusnäytöllä varustetun tilausautomaatin.<br><br> – ”Tervetuloa Digiway-ravintolaan.” Tilausautomaatti toivottaa hieman robottimaisella äänellä.<br><br> – ”Ole hyvä ja valitse näytöltä haluamasi sämpylä, täytteet sekä niiden kokoamisjärjestys.”<br><br> Doge tutkailee hetken tilausautomaatin huiman laajaa valikoimaa ja alkaa pian antamaan koneelle ohjeita herkkusämpylänsä kokoamista varten.",
-        "Lopuksi Doge painaa tilausautomaatin ”Suorita”-nappia seuraa ihmeisään, kuinka Digiwayn sämpyläkone valmistaa sämpylän juuri Dogen ohjeiden mukaan. Sämpylä näyttää valmistuvan kuin itsestään Dogen odottaessa ruokaansa tiskillä.<br><br> Pian sämpyläkoneen teleskooppikoura ojentaakin valmiin sämpylän Dogelle:<br><br> – ”Olkaa hyvä. Tilauksenne on nyt valmis. Saisiko olla vielä muuta?”, robotti kysyy ystävällisesti Dogelta.<br><br> – ”Kiitos teille oikein paljon. Lasi jäävettä voisi maistua herkkuleivän kanssa.”, Doge vastasi.<br><br> Ja silmänräpäyksessä robotti toi Dogen tarjottimelle ison mukillisen jäävettä ja jätti tämän jälkeen ystävämme nauttimaan hyvin ansaitusta herkkuleivästään.",
+        "1.\n\n 2.\n\n 3.\n\n 4.\n\n 5. ….",
+        "Lisää lopuksi SUORITA -käsky.\n\n Kokeile kaverin kanssa leikkien, toimisiko ohjekoodisi.\n\n Palauta ohjekoodi kirjoitettuna opettajalle.",
+        "Dogen herkkuleipä Kaikkihan tietävät koirien olevan aikamoisia herkkusuita ja näin on asian laita myös Dogen kohdalla. Dogen viikon kohokohta on, kun hän pääsee Apin kanssa Digiway-sämpyläravintolaan, jossa hän saa tilata juuri sellaisen sämpylän, kun kulloinkin haluaa. Sattumoisin tänään onkin Dogen nimipäivä ja Appi on luvannut viedä Dogen tämän lempiravintolaan.\n\n Huomattavaa tässä ravintolassa on se, että jokainen asiakas ohjelmoi sämpylärobotin toimimaan juuri haluamallaan tavalla, jotta tilattu leipä on juuri halutun kaltainen. Tämä vaatiikin huomattavaa tarkkuutta ja ohjeiden antamista mahdollisimman selkeällä tavalla. Doge saapuu Apin kanssa Digiway-sämpyläravintolaan ja kohtaa heti sisään päästyään suurella kosketusnäytöllä varustetun tilausautomaatin.\n\n – ”Tervetuloa Digiway-ravintolaan.” Tilausautomaatti toivottaa hieman robottimaisella äänellä.\n\n – ”Ole hyvä ja valitse näytöltä haluamasi sämpylä, täytteet sekä niiden kokoamisjärjestys.”\n\n Doge tutkailee hetken tilausautomaatin huiman laajaa valikoimaa ja alkaa pian antamaan koneelle ohjeita herkkusämpylänsä kokoamista varten.",
+        "Lopuksi Doge painaa tilausautomaatin ”Suorita”-nappia seuraa ihmeisään, kuinka Digiwayn sämpyläkone valmistaa sämpylän juuri Dogen ohjeiden mukaan. Sämpylä näyttää valmistuvan kuin itsestään Dogen odottaessa ruokaansa tiskillä.\n\n Pian sämpyläkoneen teleskooppikoura ojentaakin valmiin sämpylän Dogelle:\n\n – ”Olkaa hyvä. Tilauksenne on nyt valmis. Saisiko olla vielä muuta?”, robotti kysyy ystävällisesti Dogelta.\n\n – ”Kiitos teille oikein paljon. Lasi jäävettä voisi maistua herkkuleivän kanssa.”, Doge vastasi.\n\n Ja silmänräpäyksessä robotti toi Dogen tarjottimelle ison mukillisen jäävettä ja jätti tämän jälkeen ystävämme nauttimaan hyvin ansaitusta herkkuleivästään.",
       ],
       numeroLista: [
         "Valitse sämpyläksi täysjyväsämpylä.",
