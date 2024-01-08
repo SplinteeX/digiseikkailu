3eb283522b383337c0a0249d14884f7abc559275
Lenska5 <lenni.latvala@gmail.com>
2024-01-08 13:05:33 +0200

    +

--- frontend/src/components/data/trolliKommentoimassaData.jsx
+++ frontend/src/components/data/trolliKommentoimassaData.jsx
@@ -3,6 +3,7 @@ export const TrolliKommentoimassaData = () => {
     "Tehtävä: Trolli valloittaa keskustelupalstan!",
     "Valeuutisia ja valittavia viestejä kirjoittava trolli on päässyt vauhtiin",
     "Se kirjoittaa nyt perättömiä uutisia ja ikäviä kommentteja keskustelupalstoille.",
+    "Minkälaisen valeuutisen tai kommenttisarjan hän olisi voinut keksiä esim. kaupungistasi?",
   ];
   return (
     <div className="normiTekstit">
