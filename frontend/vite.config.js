2f1a097b4e1ffa5f79eecd660efc0a31d92c26d7
Jesper <88264190+SplinteeX@users.noreply.github.com>
2024-01-21 13:30:13 +0200

    Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(
-        "/etc/letsencrypt/live/digiadventurers.com/privkey.pem"
-      ),
-      cert: fs.readFileSync(
-        "/etc/letsencrypt/live/digiadventurers.com/fullchain.pem"
-      ),
-    },
-  },
-  plugins: [react()],
-});
+export default ({ command }) => {
+  if (command === "server") {
+    return defineConfig({
+      server: {
+        https: {
+          key: fs.readFileSync(
+            "/etc/letsencrypt/live/digiadventurers.com/privkey.pem"
+          ),
+          cert: fs.readFileSync(
+            "/etc/letsencrypt/live/digiadventurers.com/fullchain.pem"
+          ),
+        },
+      },
+      plugins: [react()],
+    });
+  } else {
+    return defineConfig({
+      plugins: [react()],
+    });
+  }
+};
