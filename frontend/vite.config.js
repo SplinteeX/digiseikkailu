c9338e7bc54d744fcb7ed9192cb30b5b46085b9e
Lenska5 <lenni.latvala@gmail.com>
2024-01-21 14:44:13 +0200

    +

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
 
-export default ({ command }) => {
-  if (command === "server") {
-    return defineConfig({
-      server: {
-        https: {
-          key: fs.readFileSync(
-            "/etc/letsencrypt/live/digiadventurers.com/privkey.pem"
-          ),
-          cert: fs.readFileSync(
-            "/etc/letsencrypt/live/digiadventurers.com/fullchain.pem"
-          ),
-        },
-      },
-      plugins: [react()],
-    });
-  } else {
-    return defineConfig({
-      plugins: [react()],
-    });
-  }
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
